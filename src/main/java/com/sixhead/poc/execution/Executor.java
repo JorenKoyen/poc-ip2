cc6f52081a56cb0316cf2ebd78f21bfef8ead302
Joren Koyen <koyenjoren@gmail.com>
2020-11-28 14:24:52 +0100

    Clean up exeuctor formatting

--- src/main/java/com/sixhead/poc/execution/Executor.java
+++ src/main/java/com/sixhead/poc/execution/Executor.java
@@ -11,33 +11,46 @@ import java.util.*;
 public class Executor {
   private final Logger log = Logger.getLogger("Executor");
   private final Map<Class<? extends GameEvent>, SpecificationHandler<? extends GameEvent>> handlers;
-  private final Queue<Tuple<? extends GameEvent, Initiator>> queue;
-  private Object[] buffer;
+  private final Deque<Tuple<? extends GameEvent, Initiator>> queue;
+  private final Object[] buffer;
   private int pointer;
 
   public Executor() {
     this.queue = new LinkedList<>();
     this.handlers = new HashMap<>();
+    this.buffer = new Object[5];
+    this.pointer = 0;
   }
 
+  // -- buffer operations -----------------------
   public void setBufferState(Object o) {
     this.buffer[this.pointer] = o;
     this.pointer = (this.pointer + 1) % this.buffer.length;
   }
-
   public Object getLastState() {
     int previousPointer = this.pointer - 1;
     previousPointer = previousPointer < 0 ? this.buffer.length + previousPointer : previousPointer;
     return this.buffer[previousPointer];
   }
 
-  public void enqueue(GameEvent event, Initiator initiator) {
+  // -- queue operations ------------------------
+  public void enqueue(GameEvent event, Initiator initiator, boolean prioritise) {
     Tuple<GameEvent, Initiator> tuple = new Tuple<>(event, initiator);
-    this.queue.offer(tuple);
+    if (prioritise) {
+      this.queue.offerFirst(tuple);
+    } else {
+      this.queue.offerLast(tuple);
+    }
+
+  }
+  public void enqueue(GameEvent event, Initiator initiator) {
+    this.enqueue(event, initiator, false);
   }
   public void enqueue(Collection<GameEvent> events, Initiator initiator) {
     events.forEach(e -> this.enqueue(e, initiator));
   }
+
+  // -- handler operations ----------------------
   public void register(SpecificationHandler<? extends GameEvent> handler) {
     this.handlers.put(handler.getGameEventType(), handler);
   }
@@ -47,10 +60,11 @@ public class Executor {
    * Main processing loop that iterates
    * over the enqueued game events
    */
-  public void start() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    // init buffer
-    this.buffer = new Object[5];
-    this.pointer = 0;
+  public void start() throws
+      NoSuchMethodException,
+      InvocationTargetException,
+      IllegalAccessException
+  {
 
     while (this.queue.peek() != null) {
       Tuple<? extends GameEvent, Initiator> tuple = this.queue.poll();
@@ -69,11 +83,11 @@ public class Executor {
       // invoke handling of event
       SpecificationHandler<?> instance = this.handlers.get(event.getClass());
       if (instance == null) throw new RuntimeException("handling of this event is not supported");
-      handle.invoke(instance, tuple.x, initiator, this);
+      handle.invoke(instance, event, initiator, this);
 
       // log buffer usage
-      log.info("buffer: " + Arrays.toString(buffer));
-      log.info("buffer pointer: " + this.pointer);
+      log.debug("buffer: " + Arrays.toString(buffer));
+      log.debug("buffer pointer: " + this.pointer);
     }
 
   }
